fc99b977858e9d3267addcc69a49682f8397f606
Antoninbln <antonin.blin@hetic.net>
2018-02-07 21:37:07 +0100

    Maj du readme et suppression du code d'aide

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
 # plateforme_hetic
 
 ##todolist
+
+###1. Connexion BDD --> Afficher du contenu []
+```
+// AFFICHER DU CONTENU DANS index.ejs
+mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
+    // Test de la connexion
+        
+    if (err) { res.render({error : err})}    // Si y'a une erreur, sa coupe le .connect()
+    else { // Connexion établie --> récupère la collection de data
+
+        db.collection('tasks').find().toArray( (err, collection) => {
+            // Test la connexion à la collection
+            if (err) { res.render('index', {error : err, data: 'Aucune tâche en cours'}) }
+            else {
+                // Connexion à la collection établie
+                res.render('index', {data: collection});
+            }
+        });
+    };
+    db.close();
+});
+```
+
+
+###2. Connexion BDD --> Injecter dans la BDD []
+```
+// AJOUTER UNE TACHE EN COURS
+
+mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
+    
+    // Test de la connexion
+    if (err) { res.render('index', {error : err})}    // Si y'a une erreur, sa coupe le .connect()
+    else {
+        db.collection('tasks').insert({
+            title: req.body.title,
+            state: true
+        }, ( err, newObject ) => {
+            if ( err ) {
+                res.render('add-task', {error: err});
+            } else {
+                res.redirect(301, "/api");
+            }
+        });
+    };
+    db.close();
+});
+```
+
+
+###3. Connexion BDD --> Supprimer du contenu []
+```
+// SUPPRIMER UNE TACHE EN COURS
+
+mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
+    // Test de la connexion
+    if (err) { res.render('index', {error : err})}    // Si y'a une erreur, sa coupe le .connect()
+    else {
+        db.collection('tasks').remove({
+            _id: ObjectId(req.params.id)
+        }, ( err, newObject ) => {
+            if ( err ) {
+                res.render('suppr-task', {error: err});
+            } else {
+                res.redirect(301, "/api");
+            }
+        });
+    };
+    db.close();
+});
+```
+
+###4. Création des pages html pour dev les fonctionnalités []
--- routes/api.js
+++ routes/api.js
@@ -17,78 +17,14 @@ const mongoServer = 'mongodb://localhost:27017/todolist';
 /*
     Def des routes
 */
-    // Afficher les tâches EN COURS
-router.get('/', (req, res) => {
-    mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
-        // Test de la connexion
-        
-        if (err) { res.render({error : err})}    // Si y'a une erreur, sa coupe le .connect()
-        else { 
-            // Connexion établie --> récupère la collection de data
-            db.collection('tasks').find().toArray( (err, collection) => {
-                // Test la connexion à la collection
-                if (err) { res.render('index', {error : err, data: 'Aucune tâche en cours'}) }
-                else {
-                    // Connexion à la collection établie
-                    res.render('index', {data: collection});
-                }
-            });
-        };
-        db.close();
-    });
-});
-
-// Ajouter une task en cours
-router.post('/add-task', (req, res) => {
-    mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
-        // Test de la connexion
-        if (err) { res.render('index', {error : err})}    // Si y'a une erreur, sa coupe le .connect()
-        else {
-            db.collection('tasks').insert({
-                title: req.body.title,
-                state: true
-            }, ( err, newObject ) => {
-                if ( err ) {
-                    res.render('add-task', {error: err});
-                } else {
-                    res.redirect(301, "/api");
-                }
-            });
-        };
-        db.close();
-    });
-});
-
-// Supprimer une task
-router.post('/suppr-task/:id', (req, res) => {
-    console.log(req.params.id);
-    mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
-        // Test de la connexion
-        if (err) { res.render('index', {error : err})}    // Si y'a une erreur, sa coupe le .connect()
-        else {
-            db.collection('tasks').remove({
-                _id: ObjectId(req.params.id)
-            }, ( err, newObject ) => {
-                if ( err ) {
-                    res.render('suppr-task', {error: err});
-                } else {
-                    res.redirect(301, "/api");
-                }
-            });
-        };
-        db.close();
-    });
-});
-
-
-// Afficher un profil 
+    // Afficher un profil 
 router.get('/voir-profil/:nom', (req, res) => {
     console.log(req.params.nom);
     res.render('voir-profil/?'+req.params.nom);
 });
 
 
-// Mon compte
+    // Mon compte
 router.get('/mon-compte', (req, res) => {
     res.render('mon-compte');
 });
