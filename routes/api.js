f69b1073a824384bdf088f8dd0363e2a8bfc06d0
Grumeleux <romain.peter3@gmail.com>
2018-02-11 15:46:33 +0100

    CONNEXION INSCRIPTION

--- routes/api.js
+++ routes/api.js
@@ -3,35 +3,47 @@
 */
 const express = require('express');
 const router = express.Router();
+var User = require('../models/user');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const ObjectId = require('mongodb').ObjectID;
-
-module.exports = router;
+// module.exports = router;
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: false}));
 
-const mongoServer = 'mongodb://localhost:27017/hetic'; // Mettre le lien vers la vraie BDD ?
+// const mongoServer = 'mongodb://localhost:27017/hetic'; // Mettre le lien vers la vraie BDD ?
+const mongoServer = 'mongodb://localhost/auth';
 
 
 /*
     Def des routes
 */
+
+
+
+
+
+
+
+
+
+
     // Accueil --> Affichage des profils 
 router.get('/', (req, res) => {
-    
+    // SI (user connected ET quiz terminée) ==> on affiche tout
+    // SI (user )
     mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
     // Test de la connexion
-        console.log(db.collection('user').find('nom'));
     if (err) { res.render({error : err})}    // Si y'a une erreur, sa coupe le .connect()
     else { // Connexion établie --> récupère la collection de data
 
-        db.collection('tasks').find().toArray( (err, collection) => {
+        db.collection('user').find().toArray( (err, collection) => {
             // Test la connexion à la collection
             if (err) { res.render('index', {error : err, data: 'Aucune tâche en cours'}) }
             else {
+                
                 // Connexion à la collection établie
                 res.render('index', {data: collection});
             }
@@ -39,14 +51,83 @@ router.get('/', (req, res) => {
     };
     db.close();
 });
-
-    console.log(req.body);
 });
 
+ 
     // Afficher un profil 
-router.get('/voir-profil/:nom', (req, res) => {
-    console.log(req.params.nom);
-    res.render('voir-profil/?' + req.params.nom);
+router.get('/voir-profil/:nom', (req, res) => { // Possibilité de récup l'ID (si jamais y'a 2 Marseille dans la BDD)
+    let targetName = req.params.nom;
+    if (targetName == 'zebi') { // Seulement si la connexion avec le BDD ne trouve rien
+        res.render('voir-profil', {error : 'Désolé mais la personne que vous cherchez n\'existe pas !', data : targetName});
+    }
+    else {
+        // 404 ici dans tous les cas autre que le profil inconnu et le cas où ça marche
+        res.render('voir-profil', {data: targetName, error: ''});
+    }
+    
+    
+});
+
+
+    // Afficher un profil sans nom
+router.get('/voir-profil/', (req, res) => { 
+    res.render('404', {data : 'L\'utilisateur que vous cherchez n\'est pas enregistré sur la plateforme'});
+});
+
+    // Inscription
+router.get('/inscription', (req, res) => {
+    res.render('inscription');
+});
+
+    // Connexion
+router.get('/connexion', (req, res) => {
+    res.render('connexion');
+});
+
+    
+
+
+
+
+
+
+
+    // quiz
+router.get('/quizz', (req, res) => {
+    // Gérer la connexion à la BDD pour récupérer toutes les questions
+    mongoose.connect(mongoServer, (err, db) => { // En fonction du déroulement on prend en param soit l'erreur, soit la BDD
+    // Test de la connexion
+    if (err) { res.render({error : err})}    // Si y'a une erreur, sa coupe le .connect()
+    else { // Connexion établie --> récupère la collection de data
+
+        db.collection('quizz').find().toArray( (err, collection) => {
+            // Test la connexion à la collection
+            if (err) { res.render('404', {error : err, data: 'Aucune tâche en cours'}) }
+            else {
+                let questionAlea = collection[Math.floor(Math.random()*collection.length)];
+                console.log(questionAlea);
+                res.render('quizz', {questions: questionAlea});
+            }
+        });
+    };
+    db.close();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+    // Validation quizz
+router.post('/send-quizz', (req, res) => {
+    console.log(req.body)
+        
+    res.render('quizz', {data : req.body});
 });
 
 
@@ -54,3 +135,20 @@ router.get('/voir-profil/:nom', (req, res) => {
 router.get('/mon-compte', (req, res) => {
     res.render('mon-compte');
 });
+
+    // 404
+router.get('/404', (req, res) => {
+    res.render('404', {data: res});
+});
+
+
+
+
+module.exports = router;
+
+
+
+
+
+
+
--- www/js/script.js
+++ www/js/script.js
@@ -1,4 +1,39 @@
-// Attendre le chargement du DOM
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('button').submit( (e) => e.preventDefault() );
-}
+angular.module('app', ['ngRoute'])
+//---------------
+// Services
+//---------------
+.factory('Users', function(){
+
+    var table = document.querySelector("table");
+    var data  = parseTable(table);
+
+    return data;
+})
+//---------------
+// Controllers
+//---------------
+.controller('UserController', ['$scope', 'Users', function ($scope, Users) {
+    $scope.users = Users;
+}])
+.controller('UserDetailCtrl', ['$scope', '$routeParams', 'Users', function ($scope, $routeParams, Users) {
+    $scope.user = Users[$routeParams.id];
+}])
+//---------------
+// Routes
+//---------------
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: '/user.html',
+        controller: 'UserController'
+    })
+    .when('/:id', {
+        templateUrl: '/userDetails.html',
+        controller: 'UserDetailCtrl'
+    });
+}]);
+
+
+document.querySelector('#zebi').addEventListener('click', function(e) {
+    e.preventDefault();
+});
